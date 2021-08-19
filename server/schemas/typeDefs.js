ae501b9a3bee8b0b5089ce28b89bea990e369d17
Chris Winters <79880339+cwinters87@users.noreply.github.com>
2021-08-19 13:44:35 -0500

    backend finished

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,5 @@
 // import the gql tagged template function
-const { gql } = require("apollo-server-express");
+const { gql } = require("apollo-server-express")
 
 // create our typeDefs
 const typeDefs = gql`
@@ -44,4 +44,4 @@ const typeDefs = gql`
 `;
 
 // export the typeDefs
-module.exports = typeDefs;
+module.exports = typeDefs
--- server/server.js
+++ server/server.js
@@ -1,19 +1,19 @@
-const express = require('express');
-const {ApolloServer} = require('apollo-server-express');
-const path = require('path');
+const express = require('express')
+const {ApolloServer} = require('apollo-server-express')
+const path = require('path')
 
-const {typeDefs, resolvers} = require('./schemas');
-const {authMiddleware} = require('./utils/auth');
-const db = require('./config/connection');
-const { start } = require('repl');
+const {typeDefs, resolvers} = require('./schemas')
+const {authMiddleware} = require('./utils/auth')
+const db = require('./config/connection')
+const { start } = require('repl')
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 const app = express();
 const server = new ApolloServer({ 
   typeDefs, 
   resolvers, 
   context: authMiddleware 
-});
+})
 
 async function startup() {
   await server.start()
@@ -23,21 +23,21 @@ async function startup() {
 startup()
 
 
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
 // Serve up static assets
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/build')))
 }
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+})
 
 db.once('open', () => {
   app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
-});
+    console.log(`API server running on port ${PORT}!`)
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+  })
+})
